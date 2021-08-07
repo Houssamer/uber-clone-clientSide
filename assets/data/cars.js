d57e83fc456e6b1768a1ff39b1385f54f2fc3f87
RIAD Houssame <riad.houssame.eleve@ehtp.ac.ma>
2021-08-07 20:13:19 +0100

    changes in user interface

--- assets/data/cars.js
+++ assets/data/cars.js
@@ -1,27 +1,24 @@
 export default [
   {
     id: '0',
-    type: 'UberX',
     latitude: 30.47627873223748, 
     longitude: -8.871699241007615,
     heading: 45,
-    image: require('../images/top-UberX.png'),
+    image: require('../images/car.png'),
   },
   {
     id: '1',
-    type: 'Comfort',
     latitude: 30.48412862053464, 
     longitude: -8.874341564871738,
     heading: 0,
-    image: require('../images/top-Comfort.png'),
+    image: require('../images/car.png'),
   },
   {
     id: '2',
-    type: 'UberXL',
     latitude: 30.473382725808257, 
     longitude: -8.880623166253155,
     heading: 75,
-    image: require('../images/top-UberXL.png'),
+    image: require('../images/car.png'),
   },
 ];
 
--- components/DestinationSearch/DestinationSearch.js
+++ components/DestinationSearch/DestinationSearch.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import CarRow from '../CarRow/CarRow';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import styles from './style';
-import types from '../../assets/data/types';
+
 
 const DestinationSearch = () => {
     return (
         <View style={styles.container}>
-            {types.map(type => <CarRow car={type} key={type.id} />)}
+            <View style={styles.upContainer}>
+                <View style={styles.square}>
+                    <Image 
+                        source={require("../../assets/images/3d_car.png")}
+                        style={styles.carImg}
+                    />
+                </View>
+                <View style={styles.price}>
+                    <Text style={styles.priceText}>Prix: 30dh</Text>
+                </View>
+            </View>
+            <View style={styles.downContainer}>
+                <TouchableOpacity style={styles.acceptButton}>
+                    <Text style={styles.acceptText}>Accepter</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.annulerButton}>
+                    <Text style={styles.annulerText}>Annuler</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
